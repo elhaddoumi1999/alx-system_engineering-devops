e0cdf65e990680b61ef372decc8ea22cf96738be
elhaddoumi <elhaddoumi99@gmail.com>
2024-02-20 00:00:47 +0100

    0x15. API

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+uses a REST API to return inforamtion about an employee given their
+employee ID
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+            "task": t.get("title"),
+            "completed": t.get("completed"),
+            "username": username
+        } for t in todos]}, jsonfile)
