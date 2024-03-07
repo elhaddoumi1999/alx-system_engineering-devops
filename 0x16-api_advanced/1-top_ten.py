3ab6365cecf1ba1ab95905d1ba403908eaaf1083
elhaddoumi <elhaddoumi99@gmail.com>
2024-03-07 10:20:24 -0800

    Update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit
-"""
+'''Get top 10 hot posts'''
+import pprint
 import requests
+
+BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
 
 def top_ten(subreddit):
-    """ Prints the titles for a given subreddit """
-    r = requests.get("https://reddit.com/r/{}.json?sort=hot&limit=10".
-                     format(subreddit), headers={"User-Agent": "custom"})
-
-    if (r.status_code == 200):
-        for i in r.json().get("data").get("children"):
-            print(i.get("data").get("title"))
-    else:
-        print("None")
+    '''Get top 10 hot posts'''
+    headers = {'User-agent': 'Unix:0-subs:v1'}
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers)
+    data = response.json().get('data', {}).get('children', {})
+    if response.status_code != 200 or not data:
+        return print("None")
+    for post in data[0:10]:
+        print(post.get('data', {}).get('title'))
