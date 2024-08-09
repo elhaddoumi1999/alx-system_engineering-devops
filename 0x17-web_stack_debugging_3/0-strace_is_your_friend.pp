f0753d5fc4fc151bf4ad581c29fd71becd681f7e
EL HADDOUMI <125677762+elhaddoumi1999@users.noreply.github.com>
2024-08-09 20:06:38 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# Fix bad file ext .phpp
-file { '/var/www/html/wp-settings.php':
-  ensure  => present
-}
--> exec { 'replace':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/bin'
-}
