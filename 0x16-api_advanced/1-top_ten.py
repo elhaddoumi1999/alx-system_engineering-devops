66d6482b48704a9be27c0540ae238d12202f63a4
EL HADDOUMI <125677762+elhaddoumi1999@users.noreply.github.com>
2024-08-14 00:33:54 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
-def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/your_username)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get("data", {}).get("children", [])
-        for post in data:
-            print(post.get("data", {}).get("title"))
-    else:
-        print(None)
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
